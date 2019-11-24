35da8335e4d163aac0a70161eadb93a8eb9ccef2
Sean <sjgallen@gmail.com>
2019-11-23 22:52:30 -0600

    updated shell command

--- kuberStart.sh
+++ kuberStart.sh
@@ -1,3 +1,5 @@
 kubectl version
 
-
+# Great way to learn about a command is to use the help flag and to pipe it into the more command.
+# This will allows you to read a large amount of output easier.
+kubectl create --help | more
