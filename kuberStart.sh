4073010b520c1a6c9e3391049d642bd6d8dc5837
Sean <sjgallen@gmail.com>
2019-11-24 21:56:21 -0600

    added more kubectl

--- kuberStart.sh
+++ kuberStart.sh
@@ -3,3 +3,13 @@ kubectl version
 # Great way to learn about a command is to use the help flag and to pipe it into the more command.
 # This will allows you to read a large amount of output easier.
 kubectl create --help | more
+
+kubectl create deployment --dry-run=true # need to add name of app and image.
+
+kubectl get deployments
+
+kubectl get pods
+
+kubectl get events
+
+kubectl config view
